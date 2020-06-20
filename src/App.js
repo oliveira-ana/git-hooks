3092aa01a896e04bdf41bc1ecde562f51ea115bd
Ana Oliveira <ana.oliveira@ingresse.com>
2020-06-20 19:04:42 -0300

    Updated: Fetch data api github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* Core Package */
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* Utilites */
+import {
+    H1,
+    List,
+    ListItem,
+    Button,
+    Container,
+} from '@ingresse/aphrodite';
+
+/* Component Itself */
+export default function App() {
+    const [ repositories, setRepositories ] = useState([]);
+
+    /*Fetch api*/
+    useEffect(() => {
+        async function fetchData () {
+            const response = await fetch('https://api.github.com/users/oliveira-ana/repos')
+            const data = await response.json();
+
+            setRepositories(data);
+        }
+        fetchData();
+    }, []);
+
+    /* Did Update */
+    useEffect(() => {
+        const filtered = repositories.filter(repo => repo.favorite);
+        document.title = `Você tem ${filtered.length} favoritos`;
+    }, [repositories]);
+
+    /* Handle id */
+    function handleFavorite(id) {
+        const newRepositories = repositories.map(repo => {
+            return repo.id === id ? {...repo, favorite:!repo.favorite} : repo
+        });
+
+        setRepositories(newRepositories);
+    }
+
+    /* render */
+    return (
+        <Container center>
+            <H1 center> Meus Repositórios</H1>
+            <List>
+                {repositories.map(repo =>(
+                    <ListItem key={repo.id}>
+                        {repo.name}
+                        {repo.favorite && <span>(Favorito)</span>}
+                        <Button
+                            link
+                            onClick={() => handleFavorite(repo.id)}>
+                            Favoritar
+                        </Button>
+                    </ListItem>
+                ))}
+            </List>
+        </Container>
+    );
 }
-
-export default App;
